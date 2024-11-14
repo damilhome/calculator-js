d6ba05994c5c4d1c80e42450bf4af68ce1504ff5
Danilo <damilhome@gmail.com>
2024-11-14 11:03:46 -0300

    feat: botão de negação

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -25,6 +25,11 @@ const zero = document.getElementById('zero');
 const comma = document.getElementById('comma');
 const equal = document.getElementById('equal');
 
+const negateDisplay = () => {
+    console.log('negate display');
+    inputOutputDisplay.textContent = Number(inputOutputDisplay.textContent) * (-1)
+}
+
 const displayNumber = (event) => {
     dataValue = event.target.dataset.value;
     if(inputOutputDisplay.textContent === '0') {
